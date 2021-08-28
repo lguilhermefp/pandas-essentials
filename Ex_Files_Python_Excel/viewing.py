7d758f0fdad58fa1e1c71a5bb6a5d3c818517b10
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-08-27 21:48:27 -0300

    another course

---
+++ Ex_Files_Python_Excel/viewing.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from openpyxl.workbook import Workbook
+
+df = pd.read_csv('Exercise Files/Names.csv', header=None)
+df.columns = ['First', 'Last', 'Address', 'City', 'State', 'Area Code', '?']
+
+wanted_values = df[['First', 'Last', 'State']]
+stored = wanted_values.to_excel('State_Location.xlsx', index=None)
