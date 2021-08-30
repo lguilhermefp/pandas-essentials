f98d0527745d4b156854cb49a0d286f468b506c3
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-08-30 00:08:46 -0300

    filter 2

--- learning/filter.py
+++ learning/filter.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 df = pd.read_csv('Exercise Files/Names.csv', header=None)
-df.columns = ['First', 'Last', 'Address', 'City', 'State', 'Area Code', '?']
+df.columns = ['First', 'Last', 'Address', 'City', 'State', 'Area Code', 'Income']
 
-print(df.loc[(df['City'] == 'Riverside') & (df['First'] == 'John')])
+df['Tax %'] = df['Income'].apply(lambda x: .15 if 10000 < x < 40000 else .2 if 40000 < x < 80000 else .25)
+
+df['Taxes Owed'] = df['Income'] * df['Tax %']
+
+to_drop = ['Area Code', 'First', 'Address']
+df.drop(columns=to_drop, inplace=True)
+
+df['Test col'] = False
+df.loc[df['Income'] < 60000, 'Test col'] = True
+print(df.groupby(['Test col']).mean().sort_values('Income'))
